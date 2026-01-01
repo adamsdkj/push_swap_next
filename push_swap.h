f73003211445b13819d588a7e01b8124b8efac94
Adam Adra <adadra@c2r3s1.42beirut.com>
2026-01-01 22:49:52 +0200

    added the add last function in the stack_init_two file

--- push_swap.h
+++ push_swap.h
@@ -33,5 +33,6 @@ void	stack_print(t_stack *head);
 void	stack_push(t_stack **head, t_stack *node);
 void	stack_free(t_stack **head);
 int		stack_size(t_stack *head);
+void	stack_add_last(t_stack **head, t_stack *node);
 
 #endif
---
+++ src/stack/stack_init_two.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_init_two.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adadra <adadra@student.42.fr>              #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-01-01 20:48:09 by adadra            #+#    #+#             */
+/*   Updated: 2026-01-01 20:48:09 by adadra           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../push_swap.h"
+
+void	stack_add_last(t_stack **head, t_stack *node)
+{
+	t_stack	*first;
+
+	first = *head;
+	if (!head || !node)
+		return ;
+	if (*head == NULL)
+	{
+		*head = node;
+		return ;
+	}
+	while ((*head)->next != NULL)
+		*head = (*head)->next;
+	(*head)->next = node;
+	*head = first;
+}
