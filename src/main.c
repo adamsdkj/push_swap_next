8dbe59e63a43b7550a357f0cdab1fece2866d4b1
Adam Adra <adadra@c2r2s7.42beirut.com>
2026-01-01 18:04:47 +0200

    stack_init
    added the functions to initialize the stack with additional function that helps us when doing the actual code

--- src/main.c
+++ src/main.c
@@ -1,36 +1,11 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adadra <adadra@student.42.fr>              #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-12-31 12:55:28 by adadra            #+#    #+#             */
-/*   Updated: 2025-12-31 12:55:28 by adadra           ###   ########.fr       */
+/*   Created: 2026-01-01 14:06:04 by adadra            #+#    #+#             */
+/*   Updated: 2026-01-01 14:06:04 by adadra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
-# include "libft/libft.h"
-
-typedef struct s_stack
-{
-	int				value;
-	struct s_stack	*next;
-}	t_stack;
-
-typedef struct s_data
-{
-	t_stack	*a;
-	t_stack	*b;
-}	t_data;
-
-t_stack	*stack_new(int value);
-void	stack_print(t_stack *head);
-void	stack_push(t_stack **head, t_stack *node);
-void	stack_free(t_stack **head);
-int		stack_size(t_stack *head);
-
-#endif
