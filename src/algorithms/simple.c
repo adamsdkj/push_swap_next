1d05395267f7f59ac8347401c3d0c45ea8c30805
Charbel Bahry <cbahry@c2r4s10.42beirut.com>
2026-01-08 01:39:38 +0200

    simple fixes

--- src/algorithms/simple.c
+++ src/algorithms/simple.c
@@ -12,8 +12,7 @@
 
 #include "../../push_swap.h"
 
-int find_min_pos(t_stack *stack)
-{
+int find_min_pos(t_stack *stack) {
   t_stack *current;
   int pos;
   int min_pos;
@@ -25,10 +24,8 @@ int find_min_pos(t_stack *stack)
   pos = 0;
   min_pos = 0;
   min = stack->value;
-  while (current)
-  {
-    if (current->value < min)
-    {
+  while (current) {
+    if (current->value < min) {
       min = current->value;
       min_pos = pos;
     }
@@ -38,49 +35,39 @@ int find_min_pos(t_stack *stack)
   return (min_pos);
 }
 
-void  rotate_to_top(t_data *stack, int pos)
-{
+void rotate_to_top(t_data *stack, int pos) {
   int size;
 
   size = stack_size(stack->a);
-  if (pos <= size/2)
-  {
-    while (pos > 0)
-    {
+  if (pos <= size / 2) {
+    while (pos > 0) {
       ra(stack, 1);
       pos--;
     }
-  }
-  else
-  {
-    while (pos < size)
-    {
+  } else {
+    while (pos < size) {
       rra(stack, 1);
       pos++;
     }
   }
 }
 
-void  selection_sort(t_data *stack)
-{
+void selection_sort(t_data *stack) {
   int min_pos;
-  
+
   if (stack_is_sorted(stack->a))
-    return ;
-  if (stack_size(stack->a) == 2)
-  {
+    return;
+  if (stack_size(stack->a) == 2) {
     sa(stack, 1);
-    return ;
+    return;
   }
-  while (stack->a != NULL)
-  {
+  while (stack->a != NULL) {
     min_pos = find_min_pos(stack->a);
     rotate_to_top(stack, min_pos);
-    pb(stack, 1);
+    pb(stack);
   }
-  while (stack->b != NULL)
-  {
-    pa(stack, 1);
+  while (stack->b != NULL) {
+    pa(stack);
   }
 }
 
--- src/operations/push.c
+++ src/operations/push.c
@@ -6,19 +6,18 @@
 /*   By: cbahry <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 16:59:44 by cbahry            #+#    #+#             */
-/*   Updated: 2026/01/05 16:59:44 by cbahry           ###   ########.fr       */
+/*   Updated: 2026/01/08 01:30:59 by cbahry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
-void  pa(t_data *stack)
-{
+void pa(t_data *stack) {
   t_stack *first_a;
   t_stack *first_b;
- 
+
   if (!stack || !stack->b)
-    return ;
+    return;
   first_a = stack->a;
   first_b = stack->b;
   stack->b = stack->b->next;
@@ -29,14 +28,13 @@ void  pa(t_data *stack)
   write(1, "pa\n", 3);
 }
 
-void  pb(t_data *stack)
-{
+void pb(t_data *stack) {
 
   t_stack *first_a;
   t_stack *first_b;
- 
+
   if (!stack || !stack->a)
-    return ;
+    return;
   first_a = stack->a;
   first_b = stack->b;
   stack->a = stack->a->next;
@@ -46,26 +44,3 @@ void  pb(t_data *stack)
   stack->stats.total_ops += 1;
   write(1, "pb\n", 3);
 }
-
-int main(void)
-{
-  t_data *stack = malloc(sizeof(t_data));
-  if (!stack)
-    return (1);
-  stack->a = stack_new(1);
-  stack_push(&stack->a, stack_new(2));
-  stack_push(&stack->a, stack_new(4));
-  // stack->b = stack_new(10);
-  // stack_push(&stack->b, stack_new(32));
-  // stack_push(&stack->b, stack_new(98));
-  stack_print(stack->a);
-  // stack_print(stack->b);
-  pa(&stack);
-  stack_print(stack->a);
-  stack_print(stack->b);
-  stack_free(&stack->a);
-  stack_free(&stack->b);
-  free(stack);
-  return (0);
-}
-
