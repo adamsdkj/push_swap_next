8b9d3162cf4e46d2e4cfe93154c6b70b2205ebe7
Adam Adra <adadra@c2r3s11.42beirut.com>
2026-01-07 18:23:37 +0200

    WIP: stack_init_two changes

--- src/stack/stack_init_two.c
+++ src/stack/stack_init_two.c
@@ -72,13 +72,3 @@ float	compute_disorder(t_stack *a)
 	return (disorder);
 }
 
-t_data	data_init(void)
-{
-	t_data	data;
-
-	data.a = NULL;
-	data.b = NULL;
-	data.bench = 0;
-	data.strategy = 3;
-	return (data);
-}
